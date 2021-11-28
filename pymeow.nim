0f08da7c618e498cb4d71daad56fea73fcc744b8
qb <saschap@mail.de>
2021-11-28 10:45:08 +0100

    multi platform progress

--- pymeow.nim
+++ pymeow.nim
@@ -2,17 +2,18 @@
   PyMeow - Python Game Hacking Library
   Meow @ 2020
 ]#
+
+import src/vector
+
 when defined(windows):
   import src/windows/[
     memory,
     misc,
     overlay,
-    vector
   ]
 
 when defined(linux):
   import src/linux/[
     memory,
-    vector
   ]
 {. warning[UnusedImport]:off .}
