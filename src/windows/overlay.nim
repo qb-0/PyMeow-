09d8f8d8cbb6e00f85122eb73eb0520ef66b617b
qb <saschap@mail.de>
2021-11-28 13:14:20 +0100

    multi platform progress

--- src/windows/overlay.nim
+++ src/windows/overlay.nim
@@ -70,26 +70,26 @@ proc overlayInit(target: string = "Fullscreen", exitKey: int32 = 0x23, borderOff
   if target != "Fullscreen":
     SetWindowPos(result.hwnd, -1, rect.left, rect.top + borderOffset, 0, 0, 0x0001)
 
-proc overlayUpdate(a: Overlay) {.exportpy: "overlay_update".} =
+proc update(a: Overlay) {.exportpy: "overlay_update".} =
   OverlayWindow.swapBuffers()
   glClear(GL_COLOR_BUFFER_BIT)
   glfwPollEvents()
 
-proc overlayDeinit(a: Overlay) {.exportpy: "overlay_deinit".} =
+proc deinit(a: Overlay) {.exportpy: "overlay_deinit".} =
   OverlayWindow.destroyWindow()
   glfwTerminate()
 
-proc overlayClose(a: Overlay) {.exportpy: "overlay_close".} = 
+proc close(a: Overlay) {.exportpy: "overlay_close".} = 
   OverlayWindow.setWindowShouldClose(true)
 
-proc overlayloop(a: Overlay, update: bool = true): bool {.exportpy: "overlay_loop".} =
+proc loop(a: Overlay, update: bool = true): bool {.exportpy: "overlay_loop".} =
   if GetAsyncKeyState(a.exitKey).bool:
-    a.overlay_close()
+    a.close()
   if update:
-    a.overlay_update()
+    a.update()
   not OverlayWindow.windowShouldClose()
 
-proc overlaySetPos(a: Overlay, x, y: int32) {.exportpy: "overlay_set_pos".} =
+proc setPos(a: Overlay, x, y: int32) {.exportpy: "overlay_set_pos".} =
   SetWindowPos(a.hwnd, -1, x, y, 0, 0, 0x0001)
 
 #[
@@ -113,10 +113,10 @@ proc fontInit(height: int32, fontName: string): Font {.exportpy: "font_init".} =
   SelectObject(result.fontHDC, hOldFont)
   discard DeleteObject(hFont)
 
-proc fontDeinit(a: Font) {.exportpy: "font_deinit".} = 
+proc deinint(a: Font) {.exportpy: "font_deinit".} = 
   glDeleteLists(a.font, 96)
 
-proc fontPrint(a: Font, x, y: float, text: string, color: array[0..2, float32]) {.exportpy: "font_print".} =
+proc print(a: Font, x, y: float, text: string, color: array[0..2, float32]) {.exportpy: "font_print".} =
   glColor3f(color[0], color[1], color[2])
   glWindowPos2f(x, y)
   glPushAttrib(GL_LIST_BIT)
@@ -124,7 +124,7 @@ proc fontPrint(a: Font, x, y: float, text: string, color: array[0..2, float32]) 
   glCallLists(cast[int32](text.len), GL_UNSIGNED_BYTE, cast[pointer](text[0].unsafeAddr))
   glPopAttrib()
 
-proc fontPrintLines(a: Font, x, y: float, lines: openArray[string], color: array[0..2, float32], offset: float32 = 2) {.exportpy: "font_print_lines".} =
+proc printLines(a: Font, x, y: float, lines: openArray[string], color: array[0..2, float32], offset: float32 = 2) {.exportpy: "font_print_lines".} =
   var yPos = y
   glColor3f(color[0], color[1], color[2])
   glPushAttrib(GL_LIST_BIT)
