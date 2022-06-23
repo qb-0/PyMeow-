3ac187da97a8fb4cbe83a969625c5b4a3fba1ea2
qb <qb@home.fritz.box>
2022-06-23 19:21:35 +0200

    skip processes without baseaddr

--- examples/linux/iter_processes.py
+++ examples/linux/iter_processes.py
@@ -3,4 +3,5 @@ from pymeow import enumerate_processes
 for proc in enumerate_processes():
     print(f"[{proc['name']}] Process ID: {proc['pid']}")
     for module, module_data in proc["modules"].items():
-        print(f"\t{module} = {hex(module_data['baseAddr'])}")
+        if 'baseAddr' in module_data:
+            print(f"\t{module} = {hex(module_data['baseAddr'])}")
